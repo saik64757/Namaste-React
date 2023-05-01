1ded56bec8156fd8aabc5e44171e0d3920bf7176
saik64757 <saik64757@gmail.com>
2023-05-01 12:22:20 +0530

    Episode 2 commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  {},
+  "Hello World from React Namaste React 🚀image.png"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
