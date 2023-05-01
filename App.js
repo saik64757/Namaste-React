ca85bd1bdc746657de555ea517983ba16fc34726
saik64757 <saik64757@gmail.com>
2023-05-01 23:33:11 +0530

    Commited before Episode 3

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {},
-  "Hello World from React Namaste React 🚀image.png"
-);
+//
+// const heading = React.createElement(
+//   "h1",
+//   {},
+//   "Hello World from React Namaste React 🚀🆗"
+// );
+
+let heading = <h1> "Hello World from React Namaste React 🚀🆗"</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
